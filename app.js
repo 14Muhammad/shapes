f4fa8ced24aeefeedfaf334abbded5087658d5eb
Muhammad Ali Makhdoom <14muhammad@gmail.com>
2018-01-22 14:22:12 +0500

    heroku settings

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) = > {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 })
 
