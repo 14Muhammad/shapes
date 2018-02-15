2395970e0af5f635d9bf1075543ccd1643f4584b
Muhammad Ali Makhdoom <14muhammad@gmail.com>
2018-02-15 18:11:18 +0500

    2 m

--- src/app/math/geometry/perimeter/problems/problem-8/problem-8.component.ts
+++ src/app/math/geometry/perimeter/problems/problem-8/problem-8.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-problem-8',
@@ -6,10 +7,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./problem-8.component.css']
 })
 export class Problem8Component implements OnInit {
+  choice: string;
+  showHint: boolean;
+  isCorrect: boolean;
+  katexOptions = {
+    displayMode: true,
+    macros: {
+      '\\RR': '\\mathbb{R}'
+    }
+  };
+  options = [
+    {
+      text: 'Equilateral Triangle',
+      isCorrect: true
+    },
+    {
+      text: 'Circle',
+      isCorrect: false
+    },
+    {
+      text: 'Square',
+      isCorrect: false
+    },
+    {
+      text: 'They have equal perimeters',
+      isCorrect: false
+    }
+  ];
 
-  constructor() { }
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
   }
 
+  toggleHint() {
+    if (this.showHint) {
+      this.showHint = false;
+    } else {
+      this.showHint = true;
+    }
+  }
+
+  showResult(e) {
+    e.source.radioGroup.disabled = true;
+    for (let i = 0; i < this.options.length; i++) {
+      if (this.choice === this.options[i].text && this.options[i].isCorrect) {
+        this.isCorrect = true;
+        break;
+      } else {
+        this.isCorrect = false;
+      }
+    }
+  }
+  goTO(path) {
+    this.router.navigate([path], { relativeTo: this.route });
+  }
 }
--- src/app/math/geometry/surface-area/problems/surface-area-problem-7/surface-area-problem-7.component.ts
+++ src/app/math/geometry/surface-area/problems/surface-area-problem-7/surface-area-problem-7.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-surface-area-problem-7',
@@ -6,10 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./surface-area-problem-7.component.css']
 })
 export class SurfaceAreaProblem7Component implements OnInit {
+  choice: string;
+  showHint: boolean;
+  isCorrect: boolean;
+  options = [
+    {
+      text: 'Cube',
+      isCorrect: false
+    },
+    {
+      text: 'Sphere',
+      isCorrect: true
+    },
+    {
+      text: 'Cylinder',
+      isCorrect: false
+    },
+    {
+      text: 'All will cool down at same rate',
+      isCorrect: false
+    }
+  ];
 
-  constructor() { }
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
   }
 
+  toggleHint() {
+    if (this.showHint) {
+      this.showHint = false;
+    } else {
+      this.showHint = true;
+    }
+  }
+
+  showResult(e) {
+    e.source.radioGroup.disabled = true;
+    for (let i = 0; i < this.options.length; i++) {
+      if (this.choice === this.options[i].text && this.options[i].isCorrect) {
+        this.isCorrect = true;
+        break;
+      } else {
+        this.isCorrect = false;
+      }
+    }
+  }
+  goTO(path) {
+    this.router.navigate([path], { relativeTo: this.route });
+  }
 }
